8975e21e5d5fab6b93ab3cf0e3b7b0855d132da7
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-03-27 10:49:32 +0300

    add cloudinary

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -32,7 +32,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # third party apps
+    'cloudinary',
+    # my apps
     'app',
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
