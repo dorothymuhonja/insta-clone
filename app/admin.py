adad650e34a2b7f5e465c8d0506ba2d197f156c9
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-03-27 15:40:06 +0300

    create admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Profile, Post, Comment, Follow
 
-# Register your models here.
+admin.site.site_header = 'Insta-Clone Admin'
+admin.site.site_title = 'Insta-Clone Admin Area'
+admin.site.index_title = 'Welcome to Insta-Clone admin area'
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Follow)
+
+
