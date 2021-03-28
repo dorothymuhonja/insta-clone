a71ef752bbc28aa7d3964972ea868ff1dd2f04fa
dorothymuhonja <dorothymuhonja7@gmail.com>
2021-03-28 23:03:36 +0300

    all

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 import os
 
 
-prod_db  =  dj_database_url.config(conn_max_age=500)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
